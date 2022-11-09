1f140c8252de8f832c637444481a6ac4c013fc2d
Яна <@LAPTOP-BI8ULKOB>
2022-11-09 16:58:17 +0400

    Добавьте файлы проекта.

---
+++ HomeWorkApp/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace HomeWorkApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lbA = new System.Windows.Forms.Label();
+            this.lbB = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbresult = new System.Windows.Forms.Label();
+            this.tbresult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(571, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нажми, для результата";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb1.Location = new System.Drawing.Point(11, 9);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(202, 25);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "Введите число А и В";
+            // 
+            // lbA
+            // 
+            this.lbA.AutoSize = true;
+            this.lbA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbA.Location = new System.Drawing.Point(12, 61);
+            this.lbA.Name = "lbA";
+            this.lbA.Size = new System.Drawing.Size(38, 22);
+            this.lbA.TabIndex = 2;
+            this.lbA.Text = "А =";
+            // 
+            // lbB
+            // 
+            this.lbB.AutoSize = true;
+            this.lbB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbB.Location = new System.Drawing.Point(11, 104);
+            this.lbB.Name = "lbB";
+            this.lbB.Size = new System.Drawing.Size(47, 25);
+            this.lbB.TabIndex = 3;
+            this.lbB.Text = "B = ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(57, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(57, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 30);
+            this.textBox2.TabIndex = 5;
+            // 
+            // lbresult
+            // 
+            this.lbresult.AutoSize = true;
+            this.lbresult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbresult.Location = new System.Drawing.Point(16, 154);
+            this.lbresult.Name = "lbresult";
+            this.lbresult.Size = new System.Drawing.Size(116, 25);
+            this.lbresult.TabIndex = 6;
+            this.lbresult.Text = "Результат ";
+            // 
+            // tbresult
+            // 
+            this.tbresult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbresult.Location = new System.Drawing.Point(138, 154);
+            this.tbresult.Name = "tbresult";
+            this.tbresult.Size = new System.Drawing.Size(418, 30);
+            this.tbresult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbresult);
+            this.Controls.Add(this.lbresult);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbB);
+            this.Controls.Add(this.lbA);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lbA;
+        private System.Windows.Forms.Label lbB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbresult;
+        private System.Windows.Forms.TextBox tbresult;
+    }
+}
+
---
+++ HomeWorkApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWorkApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            string b = textBox2.Text;
+            double A = Convert.ToDouble(a);
+            double B = Convert.ToDouble(b);
+            double C = Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
+            C = Math.Round(C * 100) / 100;
+
+
+            
+            if (A == B)
+            {
+                tbresult.Text = C.ToString() + " Это равнобедренный треугольник.";
+            }
+            else if (A != B && A != C)
+            {
+                tbresult.Text = C.ToString() + " Это разносторонний треугольник.";
+            }
+            
+
+
+
+
+        }
+    }
+}
